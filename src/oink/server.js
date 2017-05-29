3620145ee2e9940870c70f31d2eef75d06331543
Mrinmayi Sadavarte <MrinmayiSadavarte>
2017-05-29 02:13:58 -0400

    Created a JavaScript API route on the server

--- src/oink/server.js
+++ src/oink/server.js
@@ -5,6 +5,19 @@ var app = express();
 
 app.use(express.static('public')); // public is the folder containing front-end stuff
 
+
+// Created an API for connecting backend to frontend
+app.get('/posts', function(req, res, next){
+	var posts = [
+		'Hello',
+		'Wassup',
+		'How are you',
+		'Goodbye',
+		'Hello again'
+	];
+	res.send(posts);
+})
+
 app.listen(3000, function () { // 3000 is the port number on which server is listening
 	console.log('Example app listening on port 3000!');
 });
